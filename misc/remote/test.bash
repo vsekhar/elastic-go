d8323f8b42144646e87e86f0ae5af4e4fedb7ff7
Vivek Sekhar <vivek@viveksekhar.ca>
2017-06-25 01:07:31 -0700

    Check output of test binary.

--- misc/remote/test.bash
+++ misc/remote/test.bash
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-set -xe
+set -e
 
 function cleanup() {
     rm -f a.out
@@ -12,4 +12,13 @@ trap cleanup EXIT
 
 # go build -buildmode=remote -o a.out -gcflags="-d remote"
 go build -o a.out
-go remote ./a.out
+OUTPUT=$(go remote ./a.out 2>&1)
+EXPECTED="var1: 1
+var2: 4
+var3: 5
+lib RemoteVar: 42"
+if [ "$OUTPUT" != "$EXPECTED" ]; then
+  echo invalid output:
+  echo $OUTPUT
+  exit 1
+fi
